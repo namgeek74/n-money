c53636a2760cf569848c49bb8548f7bef5b6d37d
namnd <namnguyennd7428@gmail.com>
2023-12-15 22:09:05 +0700

    test connection to sql server

--- src/main/java/nmoney/com/controller/WelcomeController.java
+++ src/main/java/nmoney/com/controller/WelcomeController.java
@@ -1,13 +1,29 @@
 package nmoney.com.controller;
 
+import nmoney.com.entity.Teacher;
+import nmoney.com.repository.TeacherRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class WelcomeController {
+    private TeacherRepository teacherRepository;
+
+    public WelcomeController(TeacherRepository teacherRepository) {
+        this.teacherRepository = teacherRepository;
+    }
 
     @GetMapping("/welcome-message")
     public String getWelcomeMessage() {
         return "Welcome to NMoney version 1!";
+    }
+
+    @GetMapping("/test-connection")
+    public List<Teacher> getAllTeachers() {
+        List<Teacher> teachers = teacherRepository.findAll();
+
+        return teachers;
     }
 }
---
+++ src/main/java/nmoney/com/entity/Teacher.java
@@ -0,0 +1,34 @@
+package nmoney.com.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Teacher {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "Age")
+    private int age;
+
+    @Column(name = "email")
+    private String email;
+}
